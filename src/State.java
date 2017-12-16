061c06a355a1483155736449ecb436b8b88aaad5
dorsharon <dor442@gmail.com>
2017-12-16 17:40:12 +0200

    dor

--- src/State.java
+++ src/State.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class State {
 
     public Grid currentGrid;
@@ -6,6 +8,29 @@ public class State {
     public State(Grid currentGrid, Player nextPlayerToPlay) {
         this.currentGrid = currentGrid;
         this.nextPlayerToPlay = nextPlayerToPlay;
+    }
+
+    public int getMinMax() {
+        List<Coordinates> possiblePlacements = currentGrid.getPossiblePlacements(nextPlayerToPlay);
+        int minMaxValue = nextPlayerToPlay == Player.BLACK ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+
+        // Go over all the possible placements for the next player to play
+        for (Coordinates c : possiblePlacements) {
+            // Evaluate the state of each possible placement.
+            State state = new State(currentGrid.applyPlacement(Player.BLACK, c), Player.WHITE);
+            int stateEvaluation = state.evaluate();
+
+            System.out.println("for " + c.getRow() + "," + c.getCol() + ": " + state.evaluate());
+
+            // Get the min/max value based on which player's turn is next.
+            if (nextPlayerToPlay == Player.BLACK && minMaxValue < stateEvaluation) {
+                minMaxValue = stateEvaluation;
+            } else if (nextPlayerToPlay == Player.WHITE && minMaxValue > stateEvaluation) {
+                minMaxValue = stateEvaluation;
+            }
+        }
+
+        return minMaxValue;
     }
 
     public int evaluate() {
